2b41ae8e1715d47afc883098b6d8b977eab9e7e5
Sam Kurtzman <kurtzma4@msu.edu>
2024-02-14 10:12:41 -0500

    Styling, markup, added images

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addShortcode("alert-form", function() {
+    return '<section class="cta"> <h3>Be the first to know.</h3> <form action="#"> <input type="text" placeholder="Enter your email address to recieve updates"> <input type="button" value="Keep in touch"> </form> </section>'
+  });
 };
